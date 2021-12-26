0c02fca852b040ec6146bf1183f7320e0c4fc5b1
Jaidip Ghosh <jaidip.ghosh@sap.com>
2021-12-27 02:33:18 +0530

    App with CRUD Operation

--- Python Flask Web Development Tutorial-Harry/app.py
+++ Python Flask Web Development Tutorial-Harry/app.py
@@ -1,10 +1,67 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:admin@localhost:5432/todo"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route("/")
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f'{self.sno} - {self.title}'
+
+
+@app.route("/", methods=['GET', 'POST'])
 def hello_world():
-    return "Hello Work"
+    if request.method == "POST":
+        title = request.form['title']
+        desc = request.form['desc']
+        # Create some Dummy Todo
+        todo = Todo(title=title, desc=desc)
+        # Commit it to Database
+        db.session.add(todo)
+        db.session.commit()
+
+    allTodos = Todo.query.all()
+    return render_template("index.html", allTodos=allTodos)
+
+
+@app.route("/update/<int:sno>", methods = ['GET', 'POST'])
+def update(sno):
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        # Create some Dummy Todo
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        # Commit it to Database
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template("updates.html", todo=todo)
+
+@app.route("/delete/<int:sno>")
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    print(f'Deleted Record for the ID: {sno}')
+    return redirect("/")
+
+@app.route("/show")
+def products():
+    allTodo = Todo.query.all()
+    return str(allTodo)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- Python Flask Web Development Tutorial-Harry/readme.md
+++ Python Flask Web Development Tutorial-Harry/readme.md
@@ -1,2 +1,29 @@
 - [Flask Documentation](https://flask.palletsprojects.com/en/2.0.x/)
 - [Flask Minimal App](https://flask.palletsprojects.com/en/2.0.x/quickstart/)
+- [Starter Template](https://getbootstrap.com/docs/5.1/getting-started/introduction/)
+- [Python SQLAchemy Config Documentation](https://flask-sqlalchemy.palletsprojects.com/en/2.x/config/)
+
+---
+- How to get all the Dependencies as a Requirement.txt
+
+```python
+python -m pip freeze > requirements.txt
+```
+
+- How to create tables mentioned in app.py ( or the Data model)
+As mentioned [here](https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/#a-minimal-application)
+
+```python
+from app import db
+db.create_all()
+```
+
+- How to query object, like mentioned [here](https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/#a-minimal-application)
+
+```python
+# To Fetch all the record
+Todo.query.all()
+
+# Fetch record based on Filter Condition
+Todo.query.filter_by(sno=1).first()
+```
