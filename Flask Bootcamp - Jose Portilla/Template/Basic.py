b6a2d1c8d416239440bc420451a3d5a93db2d530
Jaidip Ghosh <jaidip1994@gmail.com>
2022-02-09 22:40:46 +0530

    Template Variable

--- Flask Bootcamp - Jose Portilla/Template/Basic.py
+++ Flask Bootcamp - Jose Portilla/Template/Basic.py
@@ -2,8 +2,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template('basic.html')
+def indecroute():
+    return "Nothing here"
+
+@app.route("/<name>")
+def index(name):
+    letters = list(name)
+    return render_template('basic.html', my_var = letters)
 
 if __name__ == '__main__':
     app.run(debug=True)
