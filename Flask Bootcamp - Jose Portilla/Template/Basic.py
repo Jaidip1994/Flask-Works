4cc0f0a2d7d39245445d30f55feefcdcb593024b
Jaidip Ghosh <jaidip1994@gmail.com>
2022-03-04 23:00:59 +0530

    Jinja Template

--- Flask Bootcamp - Jose Portilla/Template/Basic.py
+++ Flask Bootcamp - Jose Portilla/Template/Basic.py
@@ -10,5 +10,10 @@ def index(name):
     letters = list(name)
     return render_template('basic.html', my_var = letters)
 
+@app.route("/jinja")
+def jinjaTemplate():
+    mylist = [1,22,3,4]
+    return render_template("basic2.html", mylist = mylist)
+
 if __name__ == '__main__':
     app.run(debug=True)
