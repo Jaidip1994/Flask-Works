3d4413a49de57a64ef42e0a218ca29eec1819556
Jaidip Ghosh <jaidip1994@gmail.com>
2022-03-06 09:37:38 +0530

    URL For Help Function

--- Flask Bootcamp - Jose Portilla/Template/TemplateInheritenceAndFilter.py
+++ Flask Bootcamp - Jose Portilla/Template/TemplateInheritenceAndFilter.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
+import os
+
 # Python Jinja Template Filters
 # https://jinja.palletsprojects.com/en/3.0.x/templates/#builtin-filters
 
@@ -12,6 +14,12 @@ def index():
 @app.route('/puppy/<name>')
 def pup_name(name):
     return render_template('puppy.html', name=name)
+
+
+@app.route('/getImage/<imgName>')
+def getImage(imgName):
+    filePath = 'static'
+    return send_file(os.path.join(filePath, imgName))
 
 if __name__ == '__main__':
     app.run(debug=True)
